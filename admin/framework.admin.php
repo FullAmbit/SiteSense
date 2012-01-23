2ec9f05063e29ff59ab8ab740e4997aeff0a6961
Austin Overton <aoverton@fullambit.com>
2012-01-23 17:05:07 -0500

    clean start

--- admin/framework.admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-function admin_buildContent($data,$db) {
-	/* this function is called to create the content for the current page */
-}
-
-function admin_content($data) {
-	/* this function is called to SHOW the content.
-}
-?>
