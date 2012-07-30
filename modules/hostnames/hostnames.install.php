197c1e60846a5cb3289143ab8ef8265033f3fa34
Shafique Arnab <sarnab@fullambit.com>
2012-07-30 14:52:49 -0400

    Fixed installer file

--- modules/hostnames/hostnames.install.php
+++ modules/hostnames/hostnames.install.php
@@ -2,7 +2,7 @@
 
 function hostnames_settings(){
 	return array(
-		'name' => 'Hostnames',
+		'name' => 'hostnames',
 		'shortName' => 'hostnames'
 	);
 }
@@ -10,13 +10,15 @@ function hostnames_settings(){
 function hostnames_install($db,$drop = FALSE){
 	$structures = array(
 		'hostnames' => array(
-			'hostname' => 'VARCHAR(64) NOT NULL DEFAULT ``',
-			'defaultTheme' => 'VARCHAR(64) NOT NULL DEFAULT ``',
-			'defaultLanguage' => 'VARCHAR(64) NOT NULL DEFAULT ``',
-			'homepage' => 'VARCHAR(64) NOT NULL DEFAULT ``'
+			'hostname' => 'VARCHAR(64) NOT NULL DEFAULT ""',
+			'defaultTheme' => 'VARCHAR(64) NOT NULL DEFAULT ""',
+			'defaultLanguage' => 'VARCHAR(64) NOT NULL DEFAULT ""',
+			'homepage' => 'VARCHAR(64) NOT NULL DEFAULT ""',
+			'PRIMARY KEY (`hostname`)'
 		)
 	);
 	$db->createTable('hostnames',$structures['hostnames'],false);
+	return NULL;
 }
 
 function hostnames_uninstall($db){
