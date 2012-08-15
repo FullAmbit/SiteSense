f0ff5bedd2e3c3c93c92c4784ce6326b026054b5
Shafique Arnab <sarnab@fullambit.com>
2012-08-15 13:33:41 -0400

    Spelling Error Fix + Alignment

--- modules/hostnames/languages/hostnames.phrases.en_us.php
+++ modules/hostnames/languages/hostnames.phrases.en_us.php
@@ -2,21 +2,21 @@
 
 function languages_hostnames_en_us(){
 	return array(
-		"warning" => "WARNING",
-		'manageHostnamesHeading' => "Manage Hostnames",
-		'hostname' => "Hostname",
-		"addHostnameSuccessMessage" => "The hostname was successfully added to the database.",
-		"captionAddHostname" => "Add A Hostname",
-		"captionEditHostname" => "Edit Hostname",
-		"editHostnameSuccessMessage" => "The changes you made were successfully saved.",
-		"hostnameNotFound" => "The hostname you specific could not be found.",
-		"deleteHostnameConfirmHeading" => "Are you sure you want to delete this hostname?",
-		"deleteHostnameConfirmMessage" => "This will also delete all associated settings and URL remaps.",
-		"deleteHostnameSuccessMessage" => "The hostname and all associated URL remaps have been deleted.",
-		"submitHostnameItemForm" => "Save Hostname",
-		"labelHostnameItemHostname" => "Hostname",
-		"labelHostnameItemDefaultTheme" => "Default Theme",
+		"warning"                          => "WARNING",
+		'manageHostnamesHeading'           => "Manage Hostnames",
+		'hostname'                         => "Hostname",
+		"addHostnameSuccessMessage"        => "The hostname was successfully added to the database.",
+		"captionAddHostname"               => "Add A Hostname",
+		"captionEditHostname"              => "Edit Hostname",
+		"editHostnameSuccessMessage"       => "The changes you made were successfully saved.",
+		"hostnameNotFound"                 => "The hostname you specified could not be found.",
+		"deleteHostnameConfirmHeading"     => "Are you sure you want to delete this hostname?",
+		"deleteHostnameConfirmMessage"     => "This will also delete all associated settings and URL remaps.",
+		"deleteHostnameSuccessMessage"     => "The hostname and all associated URL remaps have been deleted.",
+		"submitHostnameItemForm"           => "Save Hostname",
+		"labelHostnameItemHostname"        => "Hostname",
+		"labelHostnameItemDefaultTheme"    => "Default Theme",
 		"labelHostnameItemDefaultLanguage" => "Default Language",
-		"labelHostnameItemHomepage" => "Homepage"
+		"labelHostnameItemHomepage"        => "Homepage"
 	);
 }
