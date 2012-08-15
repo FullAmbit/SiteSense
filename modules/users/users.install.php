9f5a345eab489435c3fa3473aebea144367e5521
Shafique Arnab <sarnab@fullambit.com>
2012-08-15 16:24:04 -0400

    Update To Make Users Compatible With Language Phrases

--- modules/users/users.install.php
+++ modules/users/users.install.php
@@ -28,139 +28,142 @@ function users_settings() {
 		'shortName' => 'users'
 	);
 }
-function users_install($db, $drop=false) {
-	$structures = array(
-		'activations' => array(
-			'userId'              => SQR_IDKey,
-			'hash'                => 'VARCHAR(255)',
-			'expires'             => SQR_time
-		),
-		'banned' => array(
-			'id'                  => SQR_IDKey,
-			'userId'              => SQR_ID,
-			'email'               => SQR_email,
-			'ipAddress'           => SQR_IP,
-			'timestamp'           => SQR_added,
-			'expiration'          => SQR_time,
-			'UNIQUE KEY `userId` (`userId`)'
-		),
-		'sessions' => array(
-			'sessionId'           => 'VARCHAR(255) NOT NULL PRIMARY KEY',
-			'userId'              => SQR_ID,
-			'expires'             => SQR_time,
-			'ipAddress'           => SQR_IP,
-			'userAgent'           => 'VARCHAR(255)',
-            //'language'            => 'VARCHAR(64) NOT NULL DEFAULT""',
-			'KEY `userId` (`userId`,`expires`)'
-		),
-		'users' => array(
-			'id'                  => SQR_IDKey,
-			'name'                => SQR_username,
-			'password'            => SQR_password,
-			'firstName'           => 'VARCHAR(31) NOT NULL',
-			'lastName'            => 'VARCHAR(31) NOT NULL',
-			'registeredDate'      => SQR_added,
-			'registeredIP'        => SQR_IP,
-			'lastAccess'          => SQR_time,
-			'contactEMail'        => SQR_email,
-			'publicEMail'         => SQR_email,
-			'emailVerified'       => SQR_boolean.' DEFAULT \'0\'',
-			'activated'           => SQR_boolean.' DEFAULT \'0\'',
-			'timeZone'     => 'VARCHAR(63) DEFAULT NULL',
-			'defaultLanguage'     => 'VARCHAR(64) NOT NULL DEFAULT ""'
-		),
-		'users_dynamic_fields' => array(
-			'id'                  => SQR_ID,
-			'userId'              => SQR_ID,
-			'name'                => 'VARCHAR(255) NOT NULL',
-			'value'               => 'VARCHAR(255) NOT NULL'
-		),
-		'user_groups' => array(
-			'userID'              => SQR_ID,
-			'groupName'           => SQR_name,
-			'expires'             => SQR_time
-		),
-		'user_group_permissions' => array(
-			'groupName'           => SQR_name,
-			'permissionName'      => SQR_name,
-			'value'               => 'TINYINT(1) NOT NULL'
-		),
-		'user_permissions' => array(
-			'userId'              => SQR_ID,
-			'permissionName'      => SQR_name,
-			'value'               => 'TINYINT(1) NOT NULL'
-		)
-	);
-	if ($drop)
-		users_uninstall($db);
+function users_install($db, $drop=false, $firstInstall = FALSE, $lang = "en_us") {
 
-	$db->createTable('activations', $structures['activations'], false);
-	$db->createTable('banned', $structures['banned'], false);
-	$db->createTable('sessions', $structures['sessions'], false);
-	$db->createTable('users', $structures['users'], false);
-	$db->createTable('user_groups', $structures['user_groups'], false);
-	$db->createTable('user_group_permissions', $structures['user_group_permissions'], false);
-	$db->createTable('user_permissions', $structures['user_permissions'], false);
-
-	// Set up default permission groups
-	$defaultPermissionGroups=array(
-		'Moderator' => array(
-			'users_access',
-			'users_accessOthers',
-			'users_activate',
-			'users_add',
-			'users_ban',
-			'users_edit',
-			'users_delete',
-			'users_groups'
-		),
-		'Writer' => array(
-			'users_access',
-			'users_edit'
-		),
-		'Blogger' => array(
-			'users_access',
-			'users_edit'
-		),
-		'User' => array(
-			'users_access'
-		)
-	);
-	foreach ($defaultPermissionGroups as $groupName => $permissions) {
-		foreach ($permissions as $permissionName) {
-			$statement=$db->prepare('addPermissionByGroupName');
-			$statement->execute(
-				array(
-					':groupName' => $groupName,
-					':permissionName' => $permissionName,
-					':value' => '1'
-				)
-			);
+	if($firstInstall){
+		$structures = array(
+			'activations' => array(
+				'userId'              => SQR_IDKey,
+				'hash'                => 'VARCHAR(255)',
+				'expires'             => SQR_time
+			),
+			'banned' => array(
+				'id'                  => SQR_IDKey,
+				'userId'              => SQR_ID,
+				'email'               => SQR_email,
+				'ipAddress'           => SQR_IP,
+				'timestamp'           => SQR_added,
+				'expiration'          => SQR_time,
+				'UNIQUE KEY `userId` (`userId`)'
+			),
+			'sessions' => array(
+				'sessionId'           => 'VARCHAR(255) NOT NULL PRIMARY KEY',
+				'userId'              => SQR_ID,
+				'expires'             => SQR_time,
+				'ipAddress'           => SQR_IP,
+				'userAgent'           => 'VARCHAR(255)',
+	            //'language'            => 'VARCHAR(64) NOT NULL DEFAULT""',
+				'KEY `userId` (`userId`,`expires`)'
+			),
+			'users' => array(
+				'id'                  => SQR_IDKey,
+				'name'                => SQR_username,
+				'password'            => SQR_password,
+				'firstName'           => 'VARCHAR(31) NOT NULL',
+				'lastName'            => 'VARCHAR(31) NOT NULL',
+				'registeredDate'      => SQR_added,
+				'registeredIP'        => SQR_IP,
+				'lastAccess'          => SQR_time,
+				'contactEMail'        => SQR_email,
+				'publicEMail'         => SQR_email,
+				'emailVerified'       => SQR_boolean.' DEFAULT \'0\'',
+				'activated'           => SQR_boolean.' DEFAULT \'0\'',
+				'timeZone'     => 'VARCHAR(63) DEFAULT NULL',
+				'defaultLanguage'     => 'VARCHAR(64) NOT NULL DEFAULT ""'
+			),
+			'users_dynamic_fields' => array(
+				'id'                  => SQR_ID,
+				'userId'              => SQR_ID,
+				'name'                => 'VARCHAR(255) NOT NULL',
+				'value'               => 'VARCHAR(255) NOT NULL'
+			),
+			'user_groups' => array(
+				'userID'              => SQR_ID,
+				'groupName'           => SQR_name,
+				'expires'             => SQR_time
+			),
+			'user_group_permissions' => array(
+				'groupName'           => SQR_name,
+				'permissionName'      => SQR_name,
+				'value'               => 'TINYINT(1) NOT NULL'
+			),
+			'user_permissions' => array(
+				'userId'              => SQR_ID,
+				'permissionName'      => SQR_name,
+				'value'               => 'TINYINT(1) NOT NULL'
+			)
+		);
+		if ($drop)
+			users_uninstall($db);
+	
+		$db->createTable('activations', $structures['activations'], false);
+		$db->createTable('banned', $structures['banned'], false);
+		$db->createTable('sessions', $structures['sessions'], false);
+		$db->createTable('users', $structures['users'], false);
+		$db->createTable('user_groups', $structures['user_groups'], false);
+		$db->createTable('user_group_permissions', $structures['user_group_permissions'], false);
+		$db->createTable('user_permissions', $structures['user_permissions'], false);
+	
+		// Set up default permission groups
+		$defaultPermissionGroups=array(
+			'Moderator' => array(
+				'users_access',
+				'users_accessOthers',
+				'users_activate',
+				'users_add',
+				'users_ban',
+				'users_edit',
+				'users_delete',
+				'users_groups'
+			),
+			'Writer' => array(
+				'users_access',
+				'users_edit'
+			),
+			'Blogger' => array(
+				'users_access',
+				'users_edit'
+			),
+			'User' => array(
+				'users_access'
+			)
+		);
+		foreach ($defaultPermissionGroups as $groupName => $permissions) {
+			foreach ($permissions as $permissionName) {
+				$statement=$db->prepare('addPermissionByGroupName');
+				$statement->execute(
+					array(
+						':groupName' => $groupName,
+						':permissionName' => $permissionName,
+						':value' => '1'
+					)
+				);
+			}
 		}
+		// ---
+		// Generate an admin account if this is a fresh installation
+		if ($db->countRows('users')==0) {
+			try {
+				$newPassword=common_randomPassword();
+				echo '
+					<h3>Attempting to add admin user</h3>';
+				$statement=$db->prepare('addUser', 'installer');
+				$statement->execute(array(
+						':name' => 'admin',
+						':passphrase' => hash('sha256', $newPassword),
+						':registeredIP' => $_SERVER['REMOTE_ADDR']
+					));
+				echo '
+					<p>Administrator account automatically generated!</p>';
+			} catch(PDOException $e) {
+				$db->installErrors++;
+				echo '
+					<h3 class="error">Failed to create administrator account!</h3>
+					<pre>', $e->getMessage(), '</pre><br />';
+			}
+		} else echo '<p class="exists">"users database" already contains records</p>';
+		return $newPassword;
 	}
-	// ---
-	// Generate an admin account if this is a fresh installation
-	if ($db->countRows('users')==0) {
-		try {
-			$newPassword=common_randomPassword();
-			echo '
-				<h3>Attempting to add admin user</h3>';
-			$statement=$db->prepare('addUser', 'installer');
-			$statement->execute(array(
-					':name' => 'admin',
-					':passphrase' => hash('sha256', $newPassword),
-					':registeredIP' => $_SERVER['REMOTE_ADDR']
-				));
-			echo '
-				<p>Administrator account automatically generated!</p>';
-		} catch(PDOException $e) {
-			$db->installErrors++;
-			echo '
-				<h3 class="error">Failed to create administrator account!</h3>
-				<pre>', $e->getMessage(), '</pre><br />';
-		}
-	} else echo '<p class="exists">"users database" already contains records</p>';
-	return $newPassword;
 }
 function users_uninstall($db) {
 	$db->dropTable('activations');
