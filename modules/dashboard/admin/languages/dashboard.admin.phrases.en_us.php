3991d72dc629b5d600865f9032c2417d3d3b7be0
Alex Stanford <astanford@fullambit.com>
2012-08-16 19:41:15 -0400

    Cleaned up formatting to match the SS development guidelines

--- modules/dashboard/admin/languages/dashboard.admin.phrases.en_us.php
+++ modules/dashboard/admin/languages/dashboard.admin.phrases.en_us.php
@@ -1,10 +1,8 @@
 <?php
-
 function languages_dashboard_admin_en_us(){
 	return array(
-		"welcomeMessageHeading"		=>			"Welcome to the Control Panel for your CMS installation",
-		"welcomeMessage"			=>			"Please choose from the options on the left to manage your settings."
+		'welcomeMessageHeading'   => 'Welcome to the Control Panel for your CMS installation',
+		'welcomeMessage'          => 'Please choose from the options on the left to manage your settings.'
 	);
 }
-
 ?>
