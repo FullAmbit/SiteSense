762e10dd44704e4aa70163765e1208403e263f9d
Zach Bloomquist <zbloomquist@fullambit.net>
2012-10-05 18:42:11 -0400

    Just kidding, that didn't work

--- index.php
+++ index.php
@@ -420,7 +420,8 @@ final class sitesense {
 		date_default_timezone_set($this->settings['defaultTimeZone']);
 		ini_set('date.timezone', $this->settings['defaultTimeZone']);
 		// Append attributions
-		$this->settings['parsedFooterContent'] .= ($this->settings['removeAttribution'] == '0') ? common_parseDynamicValues($this, '|attribution|') : '';
+		$attribution='|attribution|';
+		$this->settings['parsedFooterContent'] .= ($this->settings['removeAttribution'] == '0') ? common_parseDynamicValues($this, $attribution) : '';
 		// Check to see if CDN plugin should be loaded
 		if ($this->settings['useCDN']=='1') {
 			common_loadPlugin($this, $this->settings['cdnPlugin']);
